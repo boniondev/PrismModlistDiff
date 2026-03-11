e416810fd9004f12861d513a9ab42edc04445831
boniondev <55188452+boniondev@users.noreply.github.com>
2026-03-11 17:17:15 +0100

    Submit button no longer reloads the page and extra JS stuff I'll use in the future

---
+++ index.js
@@ -0,0 +1,8 @@
+let listInputForm         = document.getElementById("listInputForm")
+let ListInputSubmitButton = document.getElementById("ListInputSubmitButton")
+let listOneInputText      = document.getElementById("listOneInputText")
+let listTwoInputText      = document.getElementById("listTwoInputText")
+
+listInputForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
