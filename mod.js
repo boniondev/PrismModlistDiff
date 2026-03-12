2698d2c86d824a42e46575a226ebf5b19a29fe92
boniondev <55188452+boniondev@users.noreply.github.com>
2026-03-12 09:23:35 +0100

    Mod class

---
+++ mod.js
@@ -0,0 +1,23 @@
+/*
+    For reference:
+
+    When Prism exports a modlist as CSV with all options ticked, it uses this format:
+    modname,downloadURL,version,authorname,more authornames...,filename
+    The number of author names is ALWAYS >= 1
+
+*/
+
+export class Mod {
+
+    constructor(linestr) {
+
+        let parsedline   = linestr.split(',')
+        this.modName     = parsedline[0]
+        this.modURL      = parsedline[1]
+        this.modVersion  = parsedline[2]
+        this.modAuthor   = parsedline[3]
+        this.modFilename = parsedline[parsedline.length - 1] // We only take the first author for simplicity
+
+    }
+
+}
