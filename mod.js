69d7d5c5c78e7bca50155ee0e9cfb3ce6c64af69
boniondev <55188452+boniondev@users.noreply.github.com>
2026-03-12 11:02:29 +0100

    bits for everyone!

--- mod.js
+++ mod.js
@@ -9,6 +9,12 @@
 
 export class Mod {
 
+    static BIT_NAME     = 1 << 0
+    static BIT_URL      = 1 << 1
+    static BIT_VERSION  = 1 << 2
+    static BIT_AUTHOR   = 1 << 3
+    static BIT_FILENAME = 1 << 4
+
     constructor(linestr) {
 
         let parsedline   = [""]
@@ -19,6 +25,40 @@ export class Mod {
         this.modAuthor   = parsedline[3]  // We only take the first author for simplicity
         this.modFilename = parsedline[parsedline.length - 1]
 
+    }
+
+    compare(mod){
+
+        let mask = 0
+
+        if (this.modName !== mod.getModName())         mask |= BIT_NAME
+        if (this.modURL !== mod.getModURL())           mask |= BIT_URL
+        if (this.modVersion !== mod.getModVersion())   mask |= BIT_VERSION
+        if (this.modAuthor !== mod.getModAuthor())     mask |= BIT_AUTHOR
+        if (this.modFilename !== mod.getModFilename()) mask |= BIT_FILENAME
+
+        return mask
+
+    }
+
+    getModName() {
+        return this.modName
+    }
+
+    getModURL() {
+        return this.modURL
+    }
+
+    getModVersion() {
+        return this.modVersion
+    }
+
+    getModAuthor() {
+        return this.modAuthor
+    }
+
+    getModFilename(){
+        return this.modFilename
     }
 
 }
