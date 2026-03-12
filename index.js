04f6c27cb59756eb268345da3331e317b70b869e
boniondev <55188452+boniondev@users.noreply.github.com>
2026-03-12 15:12:08 +0100

    Let's see if this works or nah

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Modlist } from "./modlist"
+
 const listInputForm         = document.getElementById("listInputForm")
 const listOneInputText      = document.getElementById("listOneInputText")
 const listTwoInputText      = document.getElementById("listTwoInputText")
@@ -8,6 +10,15 @@ listInputForm.addEventListener("submit", (e) => {
 
     if (listOneInputText.textContent == '' && listTwoInputText.textContent == ''){
         // Cry about it here
+    }
+
+    const targetModList  = new Modlist(listOneInputText.textContent)
+    const currentModList = new Modlist(listTwoInputText.textContent)
+
+    const modReports = targetModList.compare(currentModList)
+
+    if (modReports.length == 0) {
+        console.log("They be the same!!!")
     }
 
 })
