a333b6d694cc7af0b06da3940d64fcb21d42b1a8
boniondev <55188452+boniondev@users.noreply.github.com>
2026-03-13 17:08:58 +0100

    Old stuff begone

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 import { Modlist } from "./modlist.js"
 import { animate } from "./libs/anime-4.3.6-modules/animation/index.js"
 
-/*
-const listInputForm         = document.getElementById("listInputForm")
-const listOneInputText      = document.getElementById("listOneInputText")
-const listTwoInputText      = document.getElementById("listTwoInputText")
-
-listInputForm.addEventListener("submit", (e) => {
-
-    e.preventDefault()
-
-    if (listOneInputText.value == '' && listTwoInputText.value == ''){
-        // Cry about it here
-    }
-
-    const targetModList  = new Modlist(listOneInputText.value)
-    const currentModList = new Modlist(listTwoInputText.value)
-
-    const modReports = targetModList.compare(currentModList)
-
-    if (modReports.length == 0) {
-        console.log("They be the same!!!")
-    } else {
-        console.log(modReports)
-    }
-
-})
-*/
-
 const inputTextWrapper = document.getElementById('inputTextWrapper')
 const inputText        = document.getElementById('inputText')
 
